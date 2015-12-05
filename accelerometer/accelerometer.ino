1ca3a222baf28f79fb45b45961a23cfe5d9e6b6b
Kahlan Gibson <kahlan.gibson@alumni.ubc.ca>
2015-12-05 11:44:23 -0800

    Beginning implementation of timer

--- accelerometer/accelerometer.ino
+++ accelerometer/accelerometer.ino
@@ -4,41 +4,75 @@
   Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
  This example code is in the public domain.
  */
-   int led_red = 8;
+
+int led_red = 8;
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   pinMode(led_red, OUTPUT);
+  timerinit();
 }
+
+/* TO DO 
+   -  interrupt for printing to serial port
+   -  timer for counting bpm
+   -  calculations for bpm
+*/
 
 // the loop routine runs over and over again forever:
 void loop() {
+  // THIS IS SET UP - only happens once
   // read the input on analog pin 0:
   int xValue = analogRead(A0);
   int yValue = analogRead(A1);
   int zValue = analogRead(A2);
+  int average;
+  int counter = 0;
+  
+  // BIG SUPERLOOP 
+  while (1) {
+    Serial.print(xValue);
+    Serial.print("\t");
+    Serial.print(yValue);
+    Serial.print("\t");
+    Serial.println(zValue);
+    
+    if ( z > 540 ){  // start with user is laying down
+      while ( xValue < average );
+      while ( xValue > average );
+      // start the timer here
+      TCCR1B |= (1 << CS10);
+      while ( xValue > average ) {
+        if ( /* overflow happened */ ){
+          // set overflow flag to 0
+          count = count + 1;
+        }
+      }
+      while ( xValue < average ) {
+        if ( /* overflow happened */ ){
+          // set overflow flag to 0
+          count = count + 1;
+        }
+      }
+      // stop the timer here
+      TCCR1B = 0;
+      // the number [count - TCNT1] has the period
+      // period = ??
+      // bpm = 1/(period/60);
+      // bpmint = bpm;
+    }
+  }
+}
 
-//  if (xValue > 900)
-//    {
-//       digitalWrite(led_red, HIGH);
-//    }
-//  else{
-//      digitalWrite(led_red, LOW);
-//  }
-//    
-  // print out the value you read:
-  //Serial.print("X value is: ");
-  Serial.print(xValue);
-  Serial.print("\t");
-  Serial.print(yValue);
-  Serial.print("\t");
-  Serial.println(zValue);
-  //Serial.print("Y value is: ");
-  //Serial.println(yValue);
-  //Serial.print("Z value is: ");
-  //Serial.println(zValue);
-  //Serial.println(" "); // Makes a space between the readings
-  delay(100);  // delay of 100 ms (0.1 second) - Gives us time to better read the Serial Monitor
-                // speed this up by lowering the number
+void timerinit(){
+  // set bits of timer to 0
+  // set mode
+  // stop timer
+  TCCR1A = 0;    // set entire TCCR1A register to 0
+  TCCR1B = 0;    // set entire TCCR1B register to 0 
+                 // (as we do not know the initial  values) 
+  TCCR1B |= (1 << CS10); // Sets bit CS10 in TCCR1B
+  
 }
