070c61209b5fc47f4b80d96730c8b087ccd01e1b
Kahlan Gibson <kahlan.gibson@alumni.ubc.ca>
2016-01-16 12:40:14 -0800

    Update to RespRate Code
    Beginning of ISR, Overflow plan

--- accelerometer/accelerometer.ino
+++ accelerometer/accelerometer.ino
@@ -1,78 +1,135 @@
 /*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
- This example code is in the public domain.
+  
  */
 
 int led_red = 8;
+int init_led = ##;
+int test_pin = ##;
+unsigned int tcnt2;
+int toggle = 0;
+float MS = 0.01;
+float period = 0;
+int xValue;
+int yValue;
+int zValue;
+int average;
 
-// the setup routine runs once when you press reset:
+// reset routine
 void setup() {
-  // initialize serial communication at 9600 bits per second:
+  // initialize serial com
   Serial.begin(9600);
+  // initialize red LED 8 as output
   pinMode(led_red, OUTPUT);
+  pinMode(init_led, OUTPUT);
+  // initialize timer to measure respiratory rate
   timerinit();
+  // initialize output pin for testing
+  pinMode(test_pin, OUTPUT);
 }
 
-/* TO DO 
-   -  interrupt for printing to serial port
-   -  timer for counting bpm
-   -  calculations for bpm
-*/
+// the timer init routine is called by setup
+// this init configures the timer 2 to overflow every millisecond
+// on overflow, the ISR will execute, incrementing the global count variable
+void timerinit(){
+  // disable the timer overflow interrupt 
+  TIMSK2 &= ~(1<<TOIE2);
+  // configure timer 2 in normal mode, no PWM
+  TCCR2A &= ~((1<<WGM21) | (1<<WGM20));
+  TCCR2B &= ~(1<<WGM22);
+  // select clock source: internal IO clock
+  ASSR &= ~(1<<AS2);
+  //disable compare match - only want overflow
+  TIMSK2 &= ~(1<<OCIE2A);
+  // configure the prescaler to cpu clock divided by 128
+  TCCR2B |= (1<<CS22) | (1<<CS20); // Set Bits
+  TCCR2B &= ~(1<<CS21); // Clear Bit
+  
+  // calculate proper value to load the timer counter
+  // (CPU frequency) / (prescaler value) = 125000 Hz = 8us
+  // (desired period) / 8us = 125
+  // MAX(uint8) + 1 - 125 = 131
+  // save value globally for later reload in ISR
+  tcnt2 = 131;
+  
+  // load and enable the timer
+  TCNT = tcnt2;
+  //TIMSK2 |= (1<< TOIE2);
+}
+
+//ISR for timer2 overflow
+ISR(TIMER2_OVF_vect){
+  // reload the timer
+  TCNT = tcnt2;
+  // write to digital output pin
+  digitalWrite(test_pin, toggle == 0 ? HIGH : LOW);
+  toggle = ~toggle;
+  // increment period by 1 ms
+  period = period + MS;
+}
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // THIS IS SET UP - only happens once
-  // read the input on analog pin 0:
-  int xValue = analogRead(A0);
-  int yValue = analogRead(A1);
-  int zValue = analogRead(A2);
-  int average;
-  int counter = 0;
-  
-  // BIG SUPERLOOP 
-  while (1) {
-    Serial.print(xValue);
-    Serial.print("\t");
-    Serial.print(yValue);
-    Serial.print("\t");
-    Serial.println(zValue);
-    
-    if ( z > 540 ){  // start with user is laying down
-      while ( xValue < average );
-      while ( xValue > average );
-      // start the timer here
-      TCCR1B |= (1 << CS10);
-      while ( xValue > average ) {
-        if ( /* overflow happened */ ){
-          // set overflow flag to 0
-          count = count + 1;
-        }
-      }
-      while ( xValue < average ) {
-        if ( /* overflow happened */ ){
-          // set overflow flag to 0
-          count = count + 1;
-        }
-      }
-      // stop the timer here
-      TCCR1B = 0;
-      // the number [count - TCNT1] has the period
-      // period = ??
-      // bpm = 1/(period/60);
-      // bpmint = bpm;
+  readPins();
+  // CASE user is laying down
+  // z should chill around 600 while others around 500
+  // use x - oscillations
+  if ( zValue > 540 ){
+    // wait while low and determine min value
+    while ( xValue < average ){ 
+      readPins();
     }
+    // wait while high and determine max value
+    while ( xValue > average ){
+      readPins();
+    }
+    // start the timer here - breath went low
+    TIMSK2 |= (1<< TOIE2);
+    // Timer is going up and counting ms - wait one period
+    while (xValue < average){
+      readPins();
+    }
+    while (xValue > average){
+      readPins();
+    }
+    // one period is passed - stop the timer and reset
+    TIMSK2 &= ~(1<<TOIE2);
+    TCNT = tcnt2;
+    // great! now number of ms passed should be in period
+    Serial.print(period);
   }
 }
 
-void timerinit(){
-  // set bits of timer to 0
-  // set mode
-  // stop timer
-  TCCR1A = 0;    // set entire TCCR1A register to 0
-  TCCR1B = 0;    // set entire TCCR1B register to 0 
-                 // (as we do not know the initial  values) 
-  TCCR1B |= (1 << CS10); // Sets bit CS10 in TCCR1B
+// reads the values of x,y,z from the analog pins
+void readPins(){
+  xValue = analogRead(A0);
+  yValue = analogRead(A1);
+  zValue = analogRead(A2);
+  //Serial.print(xValue);
+  //Serial.print("\t");
+  //Serial.print(yValue);
+  //Serial.print("\t");
+  //Serial.println(zValue);
+}
+
+// initializes the pins such that we have a beginning average value
+// should be around 500
+void initializePinsx(){
+  digitalWrite(init_led, HIGH);
+  int xValMax = 0;
+  int xValMin = 1024;
   
+  readPins();
+  // wait before starting
+  while(xValue > 500);
+  while(xValue < 500);
+  
+  //start measuring average
+  while(xValue > 500){
+    readPins();
+    if(xValue > xValMax){
+      xValMax = xValue;
+    }
+  }
+  while(xValue < 500);
+  digitalWrite(init_led, LOW);
 }
